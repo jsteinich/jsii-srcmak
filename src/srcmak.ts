87a09cde7bf6e1bb3eee1afd4baf00e74d2792f0
Jon Steinich <jsteinich@gmail.com>
2020-08-04 18:16:09 -0500

    fix whitespace

--- src/srcmak.ts
+++ src/srcmak.ts
@@ -17,33 +17,33 @@ export async function srcmak(srcdir: string, options: Options = { }) {
 
   const workdir = srcdir;
   //await mkdtemp(async workdir => {
-    // copy sources to temp directory
-    // await fs.copy(srcdir, workdir);
+  // copy sources to temp directory
+  // await fs.copy(srcdir, workdir);
 
-    // perform jsii compilation
-    await compile(workdir, options);
+  // perform jsii compilation
+  await compile(workdir, options);
 
-    // extract .jsii if requested
-    if (options.jsii) {
-      await fs.copy(path.join(workdir, '.jsii'), options.jsii.path);
-    }
+  // extract .jsii if requested
+  if (options.jsii) {
+    await fs.copy(path.join(workdir, '.jsii'), options.jsii.path);
+  }
 
-    // run pacmak to generate code
-    await exec(pacmakModule, [ '--code-only' ], { cwd: workdir });
+  // run pacmak to generate code
+  await exec(pacmakModule, [ '--code-only' ], { cwd: workdir });
 
-    // extract code based on selected languages
-    if (options.python) {
-      const reldir = options.python.moduleName.replace(/\./g, '/'); // jsii replaces "." with "/"
-      const source = path.resolve(path.join(workdir, 'dist/python/src', reldir));
-      const target = path.join(options.python.outdir, reldir);
-      await fs.move(source, target, { overwrite: true });
-    }
+  // extract code based on selected languages
+  if (options.python) {
+    const reldir = options.python.moduleName.replace(/\./g, '/'); // jsii replaces "." with "/"
+    const source = path.resolve(path.join(workdir, 'dist/python/src', reldir));
+    const target = path.join(options.python.outdir, reldir);
+    await fs.move(source, target, { overwrite: true });
+  }
     
-    if (options.java) {
-      const source = path.resolve(path.join(workdir, 'dist/java/src/'));
-      const target = path.join(options.java.outdir, 'src/');
-      await fs.mkdirp(target); // make sure target directory exists
-      await ncp(source, target, { clobber: false });
-    }
+  if (options.java) {
+    const source = path.resolve(path.join(workdir, 'dist/java/src/'));
+    const target = path.join(options.java.outdir, 'src/');
+    await fs.mkdirp(target); // make sure target directory exists
+    await ncp(source, target, { clobber: false });
+  }
   //});
 }
